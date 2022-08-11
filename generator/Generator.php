e922c4a999e5dff221ee80ebd429cb5fa8b9a2e8
Sergey Nikitin <kar-gi-kar@mail.ru>
2022-08-11 14:56:01 +0000

    Readme updated, form translated and deleted AutoloadExample.php.

--- generator/Generator.php
+++ generator/Generator.php
@@ -6,8 +6,8 @@ use phpDocumentor\Reflection\Types\Self_;
 use yii\gii\CodeFile;
 
 /**
- * Генератор моделей для Yii2. Добавлено приведение имени таблиуы
- * к строчным буквам, и удаление префиксов таблиц.
+ * Генератор моделей для Yii2. Изменена структура генерируемых папок - добавлено (опционально)
+ * генерация специального класса для бизнес-кода.
  * @package master_kkm\gii\generators\model
  */
 class Generator extends \yii\gii\generators\model\Generator
@@ -31,8 +31,9 @@ class Generator extends \yii\gii\generators\model\Generator
     public function getDescription()
     {
         return 'Этот генератор, аналогично стандартному, создает модель на базе класса ActiveRecord для заданной '
-            . 'таблицы базы данных. Отличие от стандартного - в обязательнром приведении имени к строчным буквам '
-            . ' и удаление префиксов.';
+            . 'таблицы базы данных. Отличие от стандартного - в герениации дополнительного класса для '
+            . 'бизнес-кода. Созданные классы наследуют от основной модели, но получают в качестве связанных '
+            . 'моделей аналогичные бизнес-модели, унаследованные от моделей связанных таблиц.';
     }
 
     /**
@@ -75,6 +76,44 @@ class Generator extends \yii\gii\generators\model\Generator
     public function hints()
     {
         return array_merge(parent::hints(), [
+            'ns' => 'Это пространство имен создаваемого класса ActiveRecord, например, <code>app\\models\\scheme</code>',
+            'db' => 'Это идентификатор компонента приложения БД.',
+            'tableName' => 'Это имя таблицы БД, с которой связан содаваемый класс ActiveRecord, например. <code>post</code>.
+                При необходимости имя таблицы может состоять из части схемы БД, например. <code>public.post</code>.
+                Имя таблицы может заканчиваться звездочкой, чтобы соответствовать нескольким именам таблиц, например: <code>tbl_*</code>
+                будет соответствовать таблицам, имя которых начинается с <code>tbl_</code>. В этом случае несколько классов ActiveRecord
+                будет сгенерировано, по одному для каждого совпадающего имени таблицы; и имена классов будут сгенерированы из
+                совпадающие символы. Например, таблица <code>tbl_post</code> сгенерирует класс <code>Post</code>.',
+            'modelClass' => 'Это имя создаваемого класса ActiveRecord. Имя класса не должно содержать
+                часть пространства имен, как указано в «Пространстве имен». Вам не нужно указывать имя класса
+                если «Имя таблицы» заканчивается звездочкой, в этом случае будет создано несколько классов ActiveRecord.',
+            'standardizeCapitals' => 'Это указывает, должны ли сгенерированные имена классов иметь стандартизированные заглавные буквы. Например,
+                имена таблиц, такие как <code>SOME_TABLE</code> или <code>Other_Table</code>, будут иметь имена классов <code>SomeTable</code>
+                и <code>OtherTable</code> соответственно. Если флажок не установлен, те же таблицы будут иметь имена классов <code>SOMETABLE</code>.
+                и <code>OtherTable</code>.',
+            'singularize' => 'Это указывает, должны ли сгенерированные имена классов быть в единственном числе. Например,
+            имена таблиц, такие как <code>some_tables</code>, будут иметь имена классов <code>SomeTable</code>.',
+            'baseClass' => 'Это базовый класс (ActiveRecord) для создаваемого класса. Это должно быть полное имя класса в пространстве имен.',
+            'generateRelations' => 'Указывает, должен ли генератор генерировать отношения на основе ограничения внешнего ключа, которые он обнаруживает
+                в базе данных. Обратите внимание, что если ваша база данных содержит слишком много таблиц, вы можете снять этот флажок, чтобы ускорить
+                процесс генерации кода.',
+            'generateJunctionRelationMode' => 'Это указывает, генерировать-ли отношения соединения с отношениями `viaTable()` или `via()` (модель перехода).
+                Убедитесь, что вы также создаете модели соединения при использовании опции «Через модель».',
+            'generateRelationsFromCurrentSchema' => 'Это указывает, должен ли генератор генерировать отношения из текущей схемы или из всех доступных схем.',
+            'generateLabelsFromComments' => 'Указывает, должен ли генератор генерировать внешние названия атрибутов, используя комментарии соответствующих столбцов БД.',
+            'useTablePrefix' => 'Это указывает, нужно ли, в создаваемом классе ActiveRecord учитывать настройку <code>tablePrefix</code> при формировании имени таблицы.
+                Например, если имя таблицы — <code>tbl_post</code> и <code>tablePrefix=tbl_</code>, класс ActiveRecord
+                вернет имя таблицы в виде <code>{{%post}}</code>.',
+            'useSchemaName' => 'Указывает, следует ли включать имя схемы в класс ActiveRecord.
+                когда он сгенерирован автоматически. Будет использоваться только схема не по умолчанию.',
+            'generateQuery' => 'Указывает, следует ли генерировать ActiveQuery для класса ActiveRecord.',
+            'queryNs' => 'Это пространство имен создаваемого класса ActiveQuery, например, <code>app\models</code>.',
+            'queryClass' => 'Это имя создаваемого класса ActiveQuery. Имя класса не должно содержать
+                часть пространства имен, указанная в «Пространстве имен ActiveQuery». Вам не нужно указывать имя класса
+                если «Имя таблицы» заканчивается звездочкой, в этом случае будет создано несколько классов ActiveQuery.',
+            'queryBaseClass' => 'Это базовый класс для создаваемого класса ActiveQuery. Это должно быть полное имя класса в пространстве имен.',
+            'enableI18N' => 'Указывает, должен ли генератор генерировать строки с помощью метода <code>Yii::t()</code>.
+                Установите для этого параметра значение <code>true</code>, если вы планируете сделать свое приложение многоязычным.',
             'businessNs' => 'Namespace, где будут располагаться модели создржащие бизнес-код.',
         ]);
     }
@@ -96,7 +135,7 @@ class Generator extends \yii\gii\generators\model\Generator
     {
         $files = [];
         $relations = $this->generateRelations();
-        $usesTable = $this->generateUsesTable($relations, $this->businessNs);
+        $usesTables = $this->generateUsesTable($relations, $this->businessNs);
         $db = $this->getDbConnection();
         foreach ($this->getTableNames() as $tableName) {
             // model:
@@ -104,6 +143,7 @@ class Generator extends \yii\gii\generators\model\Generator
             $queryClassName = $this->generateQuery ? $this->generateQueryClassName($modelClassName) : false;
             $tableRelations = isset($relations[$tableName]) ? $relations[$tableName] : [];
             $tableSchema = $db->getTableSchema($tableName);
+            $tableUse = isset($usesTables[$tableName]) ? $usesTables[$tableName] : [];
             $params = [
                 'tableName' => $tableName,
                 'className' => $modelClassName,
@@ -114,7 +154,7 @@ class Generator extends \yii\gii\generators\model\Generator
                 'rules' => $this->generateRules($tableSchema),
                 'relations' => $tableRelations,
                 'relationsClassHints' => $this->generateRelationsClassHints($tableRelations, $this->generateQuery),
-                'usesTable' => $usesTable,
+                'usesTable' => $tableUse,
             ];
             $files[] = new CodeFile(
                 \Yii::getAlias('@' . str_replace('\\', '/', $this->ns)) . '/' . $modelClassName . '.php',
@@ -151,10 +191,10 @@ class Generator extends \yii\gii\generators\model\Generator
     public function generateUsesTable(array $relations, string $nameSpace): array
     {
         $usesTable = [];
-        foreach ($relations as $relation) {
+        foreach ($relations as $tableName => $relation) {
             foreach ($relation as $item) {
                 $className = $item[1];
-                $usesTable[$className] = "$nameSpace\\$className";
+                $usesTable[$tableName][$className] = "$nameSpace\\$className";
             }
         }
         return $usesTable;
